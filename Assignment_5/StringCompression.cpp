376332eb9115cd82f28369c0eb143d1762648f36
Intezar.Akhtar <intezarakhtar@gmail.com>
2024-02-29 12:29:55 +0530

    Second commit

--- Assignment_5/StringCompression.cpp
+++ Assignment_5/StringCompression.cpp
@@ -1,1 +1,34 @@
 #include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        if (chars.size() < 2)
+            return chars.size();
+
+        int i = 0; // used to overwrite the chars array
+        int j = 0; // used to traverse the chars array 
+
+        while (j < chars.size()) {
+            chars[i] = chars[j];
+            int count = 0;
+            while (j < chars.size() && chars[j] == chars[i]) {
+                count++;
+                j++;
+            }
+
+            if (count == 1) {
+               i++; 
+            } else {
+                string str = to_string(count);
+                i++;
+                for (char& digit : str) {
+                    chars[i++] = digit;
+                }
+            }
+        }
+        return i;
+    }
+};
